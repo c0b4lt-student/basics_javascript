7f41cd2af7dd20df2c66a8f44d360ed9087f331f
crocospare <crocospare@crocospare.lan>
2021-11-16 14:58:41 +0100

    Clean branch for object and arrays final exo

--- Basics/back/operateur_de_comparaison.js
+++
@@ -1,9 +0,0 @@
-const car = {
-    type: 'BMW',
-    color:'blue',
-    doors: 3,
-    airConditioner: true
-};
-
-const {type, color, doors, airConditioner} = car;
-const isCarValid = type && color && doors && airConditioner;

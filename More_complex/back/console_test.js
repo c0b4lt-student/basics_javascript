dccf6598846291bf573a2b9af557cf946410b73b
crocospare <crocospare@crocospare.lan>
2021-11-18 14:32:41 +0100

    First commit of dates

--- More_complex/back/console_test.js
+++
@@ -1,9 +0,0 @@
-let uname = prompt('Quel est votre id ?');
-is_confirmed = confirm('Votre id est bien ' + uname + ' ?');
-if (is_confirmed) {
-  alert(`Connexion de ${uname}`)
-}
-else {
-  alert(`Deconnexion de ${uname}`)
-}
-console.log(uname);

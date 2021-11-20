26845ecca85cefb64f02b6cfbac1a351fceca42a
crocospare <crocospare@crocospare.lan>
2021-11-20 14:50:01 +0100

    last commit

--- More_complex/back/array_method.js
+++ More_complex/back/array_method.js
@@ -4,8 +4,14 @@ function isTextLengthGreaterThan5(element) {
   return element.length > 5
 }
 
-console.log(webBrowsers.every(isTextLengthGreaterThan5));
+/*console.log(webBrowsers.every(isTextLengthGreaterThan5));
 console.log(webBrowsers.sort());
 const webBrowsersFiltred = webBrowsers.filter(isTextLengthGreaterThan5);
 console.log(webBrowsersFiltred);
-console.log(webBrowsersFiltred.join('-'));
+console.log(webBrowsersFiltred.join('-')); */
+
+let numbers = [98, 100, 102, 500, 10, '97', 'bonjour'];
+
+let numbersFiltred = numbers.filter((cell) => parseInt(cell) <= 100)
+
+console.log(numbersFiltred);
